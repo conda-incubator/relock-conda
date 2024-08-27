ddc6264c0cb9ec6ad21e1056d409238beb6c6a9e
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-08-27 15:12:28 +0200

    feat: add first version of action (#1)
    * feat: add first vcersion of action
    
    * doc: added tests badge
    
    * feat: make sure works without existing lock file
    
    * doc: more text
    
    * chore: add pre-commit
    
    * chore: get pre-commit running
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * doc: add pre-commit badge
    
    * test: try a simple smoke test
    
    * test: try this
    
    * test: maybe need v4
    
    * test: ensure have a proper base
    
    * fix: wrong input
    
    * fix: wrong name
    
    * fix: wrong input again
    
    * fix: ensure token has permissions
    
    * fix: use a pat
    
    * fix: bugs
    
    * test: see what is here
    
    * try this again
    
    * fix: skip if did not relock
    
    * fix: ruff it up
    
    * what is here
    
    * test: fix the tests again and add outputs
    
    * test: do not run on own PRs
    
    * test: add test for no relocking
    
    * final bit of cleanup
    
    * test: try again
    
    * test: only run one at a time
    
    * try this
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # relock-conda
+[![tests](https://github.com/beckermr/relock-conda/actions/workflows/tests.yml/badge.svg)](https://github.com/beckermr/relock-conda/actions/workflows/tests.yml) [![pre-commit.ci status](https://results.pre-commit.ci/badge/github/beckermr/relock-conda/main.svg)](https://results.pre-commit.ci/latest/github/beckermr/relock-conda/main)
+
 GitHub action to relock conda environments using conda-lock
+
+This action runs [conda-lock](https://github.com/conda/conda-lock) to relock a conda environment and makes a PR
+with the changes if any are found. By default, a PR is only made if a package list in the input `environment.yml`
+file is updated.
+
+## Usage
+
+```yaml
+name: relock conda
+on:
+  workflow_dispatch:
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    name: test
+    steps:
+      - name: run
+        uses: beckermr/relock-conda@main
+        with:
+          # A GitHub personal access token is required
+          github-token: ${{ secrets.GITHUB_PAT }}
+
+          # files to relock w/ conda-lock
+          # environment-file: environment.yml  # default
+          # lock-file: conda-lock.yml  # default
+
+          # optional list of packages whose changes are ignore when relocking
+          # ignored-packages: "numpy,scipy"
+
+          # whether to relock on an update to any package in the environment, not just those in the environment file
+          # relock-all-packages: false  # default
+
+          # automerge the PR - you need to have GitHub automerge enabled to use this
+          # automerge: false  # default
+```
+
+See the [action.yml](action.yml) for details on possible inputs and options.
---
+++ relock.py
@@ -0,0 +1,158 @@
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+
+import click
+from conda.models.match_spec import MatchSpec
+from ruamel.yaml import YAML
+
+yaml = YAML(typ="safe", pure=True)
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.default_flow_style = False
+
+
+def _lock_to_ver(lock, platform):
+    pkg_to_ver = {}
+    for pkg in lock["package"]:
+        if pkg["platform"] == platform:
+            pkg_to_ver[pkg["name"]] = pkg["version"]
+    return pkg_to_ver
+
+
+def _reformat_lockfile(lockfile):
+    # load / dump the lockfile to make sure it is sorted
+    # so we get nice diffs
+    with open(lockfile) as f:
+        new_lock = yaml.load(f)
+    new_lock["package"] = sorted(
+        new_lock["package"], key=lambda x: (x["name"], x["platform"])
+    )
+    with open(lockfile, "w") as f:
+        yaml.dump(new_lock, f)
+
+    with open(lockfile) as f:
+        lines = [line.rstrip() for line in f]
+
+    with open(lockfile, "w") as f:
+        f.write("\n".join(lines) + "\n")
+
+
+@click.command()
+@click.option("--environment-file", required=True, type=click.Path(exists=True))
+@click.option("--lock-file", required=True, type=click.Path())
+@click.option("--ignored-packages", required=True, type=str)
+@click.option("--relock-all-packages", required=True, type=str)
+def main(environment_file, lock_file, ignored_packages, relock_all_packages):
+    relocked = False
+    with tempfile.TemporaryDirectory() as tmpdir:
+        try:
+            ignored_packages = [pkg.strip() for pkg in ignored_packages.split(",")]
+            relock_all_packages = relock_all_packages.lower() == "true"
+
+            have_existing_lock_file = os.path.exists(lock_file)
+
+            backup_lock_file = os.path.join(tmpdir, os.path.basename(lock_file))
+            if have_existing_lock_file:
+                shutil.move(lock_file, backup_lock_file)
+            else:
+                print(
+                    "No existing lock file found. Creating a new one.",
+                    flush=True,
+                    file=sys.stderr,
+                )
+
+            print("Relocking environment.yml...", flush=True, file=sys.stderr)
+            subprocess.run(
+                ["conda", "lock", "--file", environment_file, "--lockfile", lock_file],
+                check=True,
+                capture_output=True,
+            )
+
+            if not have_existing_lock_file:
+                print(
+                    "A lock file has been created in this PR since no existing one was found.",
+                    flush=True,
+                )
+                relocked = True
+            else:
+                with open(environment_file) as f:
+                    envyml = yaml.load(f)
+
+                with open(backup_lock_file) as f:
+                    old_lock = yaml.load(f)
+
+                with open(lock_file) as f:
+                    new_lock = yaml.load(f)
+
+                old_platform_pkg_to_ver = {
+                    platform: _lock_to_ver(old_lock, platform)
+                    for platform in envyml["platforms"]
+                }
+
+                new_platform_pkg_to_ver = {
+                    platform: _lock_to_ver(new_lock, platform)
+                    for platform in envyml["platforms"]
+                }
+
+                if relock_all_packages:
+                    deps_to_relock = set()
+                    for platform in envyml["platforms"]:
+                        for pkg in new_platform_pkg_to_ver[platform]:
+                            deps_to_relock.add(pkg)
+                else:
+                    deps_to_relock = set()
+                    for _spec in envyml["dependencies"]:
+                        deps_to_relock.add(MatchSpec(_spec).name)
+
+                deps_to_relock = deps_to_relock - set(ignored_packages)
+
+                relock_tuples = {platform: [] for platform in envyml["platforms"]}
+                for pkg in deps_to_relock:
+                    for platform in envyml["platforms"]:
+                        if old_platform_pkg_to_ver[platform].get(
+                            pkg
+                        ) != new_platform_pkg_to_ver[platform].get(pkg):
+                            relock_tuples[platform].append(
+                                (
+                                    pkg,
+                                    old_platform_pkg_to_ver[platform].get(pkg),
+                                    new_platform_pkg_to_ver[platform].get(pkg),
+                                )
+                            )
+
+                if any(relock_tuples[platform] for platform in envyml["platforms"]):
+                    _reformat_lockfile(lock_file)
+
+                    print("The following packages have been updated:\n", flush=True)
+                    for platform in envyml["platforms"]:
+                        print(f"  platform: {platform}", flush=True)
+                        for pkg, old_ver, new_ver in relock_tuples[platform]:
+                            print(f"    - {pkg}: {old_ver} -> {new_ver}", flush=True)
+                        print("", flush=True)
+
+                    relocked = True
+                else:
+                    print("No packages have been updated.", flush=True, file=sys.stderr)
+                    shutil.move(backup_lock_file, lock_file)
+                    relocked = False
+        except Exception as e:
+            if os.path.exists(backup_lock_file) and have_existing_lock_file:
+                shutil.move(backup_lock_file, lock_file)
+
+            subprocess.run(
+                'echo "relocked=false" >> "$GITHUB_OUTPUT"',
+                shell=True,
+            )
+
+            raise e
+
+    subprocess.run(
+        f'echo "relocked={"true" if relocked else "false"}" >> "$GITHUB_OUTPUT"',
+        shell=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
