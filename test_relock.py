adb8326a1ee3b23f85cfea7b37f9927f76a1aedf
beckermr <becker.mr@gmail.com>
2024-08-31 12:22:11 -0500

    test: better tests

--- test_relock.py
+++ test_relock.py
@@ -5,9 +5,12 @@ import pytest
 
 @pytest.mark.parametrize("input,output", [
     ("", []),
+    ("\n,\n\n", []),
+    (",", []),
+    ("\n", []),
     ("conda", ["conda"]),
     ("conda,python", ["conda", "python"]),
-    ("conda, python\nblah,blah", ["conda", "python", "blah", "blah"]),
+    ("\nconda, python\nblah,blah\n,\n\n", ["conda", "python", "blah", "blah"]),
 ])
 def test_split_package_list(input, output):
     assert _split_package_list(input) == output
