da9efa636d9df60d381aa7e7b6c98c808c071be7
beckermr <becker.mr@gmail.com>
2024-08-31 12:31:11 -0500

    fix: ruff it up

--- test_relock.py
+++ test_relock.py
@@ -3,14 +3,17 @@ from relock import _split_package_list
 import pytest
 
 
-@pytest.mark.parametrize("input,output", [
-    ("", []),
-    ("\n,\n\n", []),
-    (",", []),
-    ("\n", []),
-    ("conda", ["conda"]),
-    ("conda,python", ["conda", "python"]),
-    ("\nconda, python\nblah,blah\n,\n\n", ["conda", "python", "blah", "blah"]),
-])
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        ("", []),
+        ("\n,\n\n", []),
+        (",", []),
+        ("\n", []),
+        ("conda", ["conda"]),
+        ("conda,python", ["conda", "python"]),
+        ("\nconda, python\nblah,blah\n,\n\n", ["conda", "python", "blah", "blah"]),
+    ],
+)
 def test_split_package_list(input, output):
     assert _split_package_list(input) == output
