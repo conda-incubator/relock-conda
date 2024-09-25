e4dfad94aad84f653e4cecad250c862de442c4e6
beckermr <becker.mr@gmail.com>
2024-09-25 11:13:55 -0400

    fix: use it as an issue

--- react.py
+++ react.py
@@ -11,10 +11,10 @@ import github
 def main(full_name, pr_number, comment_id):
     gh = github.Github(auth=github.Auth.Token(os.environ["GH_TOKEN"]))
     repo = gh.get_repo(full_name)
-    pr = repo.get_pull(pr_number)
-    comment = pr.get_comment(comment_id)
+    pr_as_issue = repo.get_pull(pr_number).as_issue()
+    comment = pr_as_issue.get_comment(comment_id)
     comment.create_reaction("rocket")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
