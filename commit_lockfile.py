ea6630733eb094769a05064557131aa7bc4f8109
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-11-25 07:28:46 -0600

    Update commit_lockfile.py

--- commit_lockfile.py
+++ commit_lockfile.py
@@ -1,3 +1,4 @@
+import json
 import os
 import subprocess
 
@@ -28,17 +29,47 @@ def _get_current_branch():
     return res.stdout.strip()
 
 
+def _get_owner_repo_branch():
+    res = subprocess.run(
+        [
+            "gh",
+            "pr",
+            "view",
+            "--json",
+            "headRefName",
+            "--json",
+            "headRepository",
+            "--json",
+            "headRepositoryOwner"
+        ],
+        text=True,
+        check=True,
+        stdout=subprocess.PIPE,
+    )
+    data = json.loads(res.stdout.strip())
+    return (
+        data["headRepositoryOwner"]["login"],
+        data["headRepository"]["name"],
+        data["headRefName"],
+    )
+
+
 @click.command()
 @click.option("--lock-file", required=True, type=click.Path())
+@click.option("--event-name", required=False, default=None, type=str)
 def main(
     lock_file,
+    event_name,
 ):
-    repo_owner, repo_name = _get_repo_owner_and_name()
-    branch = _get_current_branch()
-    print(
-        f"Updating '{lock_file}' in '{repo_owner}/{repo_name}' on branch '{branch}'...",
-        flush=True,
-    )
+    if event_name == "issue_comment":
+        repo_owner, repo_name, branch = _get_owner_repo_branch()
+    else:
+        repo_owner, repo_name = _get_repo_owner_and_name()
+        branch = _get_current_branch()
+        print(
+            f"Updating '{lock_file}' in '{repo_owner}/{repo_name}' on branch '{branch}'...",
+            flush=True,
+        )
 
     gh = github.Github(auth=github.Auth.Token(os.environ["GH_TOKEN"]))
     repo = gh.get_repo(f"{repo_owner}/{repo_name}")
